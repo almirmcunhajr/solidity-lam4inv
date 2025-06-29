1d17ed7f013c053a5e143108fe064a4d798c7cf5
Almir Cunha <almir.cunha@incognia.com>
2025-06-29 12:45:06 -0300

    Implement _get_loop_ssa_maps

--- src/vc/generator.py
+++ src/vc/generator.py
@@ -1,10 +1,11 @@
 from typing import Optional
+from itertools import chain
 from slither.slither import Slither
 from slither.core.declarations import Contract, Function
-from slither.core.source_mapping.source_mapping import SourceMapping
-from slither.core.cfg.node import Constant, Node
+from slither.core.cfg.node import Constant, Node, Variable
 from slither.core.dominators.utils import compute_dominators
-from utils.slither import preorder_traversal
+from slither.slithir.variables.variable import Variable
+from slither.slithir.operations import OperationWithLValue
 
 class Generator:
     def __init__(self, file_path: str):
@@ -30,8 +31,9 @@ class Generator:
         cond_node, end_node = loops[loop_index]
 
         pre_path, trans_path, post_path = self._get_paths(function, cond_node, end_node)
-        
+
         ssa_vars = self._get_ssa_vars()
+        loop_vars, entry_ssa_map, exit_ssa_map = self._get_loop_ssa_maps(function, contract)
 
     def _get_loop_nodes(self, function: Function) -> list[tuple[Node, Node]]:
         loops = []
@@ -67,17 +69,66 @@ class Generator:
         ssa_vars = set()
         for node in function.nodes:
             for ir in node.irs_ssa:
-                if hasattr(ir, 'lvalue') and ir.lvalue and not isinstance(ir.lvalue, Constant):
+                if isinstance(ir, OperationWithLValue) and ir.lvalue and not isinstance(ir.lvalue, Constant):
                     ssa_vars.add(self._var_to_smt(ir.lvalue))
-                for var in ir.read:
-                    if not isinstance(var, Constant):
-                        ssa_vars.add(self._var_to_smt(var))
+                if hasattr(ir, 'read') and ir.read:
+                    for var in ir.read:
+                        if not isinstance(var, Constant):
+                            ssa_vars.add(self._var_to_smt(var))
         return sorted(list(ssa_vars))
 
-    def _var_to_smt(self, var: SourceMapping) -> str:
+    def _var_to_smt(self, var: Variable) -> str:
         if isinstance(var, Constant):
             return str(var.value)
         return str(var)
+
+    def _get_loop_ssa_maps(self, trans_path: list[Node], function: Function, contract: Contract) -> tuple[list[str], dict[str,str], dict[str,str]]:
+        loop_var_names: set[str] = {v.name for v in chain(function.parameters, function.returns) if v.name}
+        loop_var_names.update({v.name for v in contract.state_variables if v.name})
+
+        exit_ssa_map: dict[str, str] = {}
+        exit_ssa_base_name = {}
+        for node in trans_path:
+            for ir in node.irs_ssa:
+                if isinstance(ir, OperationWithLValue) and ir.lvalue:
+                    base_name = self._get_base_name(ir.lvalue)
+                    if not base_name:
+                        raise NoneTypeVarName()
+                    loop_var_names.add(base_name)
+                    exit_ssa_map[base_name] = self._var_to_smt(ir.lvalue)
+                    exit_ssa_base_name[str(ir.lvalue)] = base_name
+
+        entry_ssa_map: dict[str, str] = {}
+        for node in trans_path:
+            for ir in node.irs_ssa:
+                if isinstance(ir, OperationWithLValue) and str(ir.lvalue) in exit_ssa_base_name:
+                    base_name = exit_ssa_base_name[str(ir.lvalue)]
+                    if hasattr(ir, 'read') and ir.read:
+                        for var in ir.read:
+                            if self._get_base_name(var) == base_name:
+                                entry_ssa_map[base_name] = self._var_to_smt(var)
+
+        unmapped_vars = {v for v in loop_var_names if v not in entry_ssa_map}
+        for node in trans_path:
+            for ir in node.irs_ssa:
+                if hasattr(ir, 'read') and ir.read:
+                    for var in ir.read:
+                        base_name = self._get_base_name(var)
+                        if base_name and base_name in unmapped_vars:
+                            smt_ssa_name = self._var_to_smt(var)
+                            entry_ssa_map[base_name] = smt_ssa_name
+                            exit_ssa_map[base_name] = smt_ssa_name
+
+        return sorted(list(loop_var_names)), entry_ssa_map, exit_ssa_map
+
+    def _get_base_name(self, var: Variable) -> str|None:
+        if isinstance(var, Constant):
+            return str(var.value)
+        if hasattr(var, 'non_ssa_version') and var.non_ssa_version:
+            return var.non_ssa_version.name
+        if var.name:
+            return var.name.split('_ssa')[0]
+        return None
 
 class ContractNotFound(Exception):
     def __init__(self, contract_name: str):
@@ -91,3 +142,6 @@ class LoopNotFound(Exception):
     def __init__(self, loop_index: int):
         super().__init__(f'Loop {loop_index} not found')
 
+class NoneTypeVarName(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
