b91d5af012f5abb3753768ef1db6d7c1b32c0ceb
Almir Cunha <almir.cunha@incognia.com>
2025-07-08 22:07:06 -0300

    Fix loop ssa maps

--- src/vc/generator.py
+++ src/vc/generator.py
@@ -3,7 +3,7 @@ import textwrap
 from itertools import chain
 from slither.slither import Slither
 from slither.core.declarations import Contract, Function
-from slither.core.cfg.node import Constant, Node, Variable
+from slither.core.cfg.node import Constant, Node, Phi, TemporaryVariable, Variable
 from slither.core.dominators.utils import compute_dominators
 from slither.slithir.convert import Unary
 from slither.slithir.variables.variable import Variable
@@ -62,10 +62,25 @@ class Generator:
         smt_trans_fun_definition = self._define_smt_semantic_fun('trans-f', trans_path)
         smt_post_fun_definition = self._define_smt_semantic_fun('post-f', post_path)
         
-        vc_base = [smt_declarations, smt_inv_fun_definition]
-        pre_vc = '\n'.join(vc_base + [smt_pre_fun_definition]) 
-        trans_vc = '\n'.join(vc_base + [smt_trans_fun_definition])
-        post_vc = '\n'.join(vc_base + [smt_post_fun_definition])
+        base = [smt_declarations, smt_inv_fun_definition]
+        entry_args = ' '.join(entry_ssa_map[v] for v in loop_vars if v in entry_ssa_map)
+        exit_args = ' '.join(exit_ssa_map[v] for v in loop_vars if v in exit_ssa_map)
+
+        pre_vc = '\n'.join(base + [
+            smt_pre_fun_definition,
+            f'(assert (not (=> pre-f (inv-f {entry_args}))))',
+            '(check-sat)'
+        ]) 
+        trans_vc = '\n'.join(base + [
+            smt_trans_fun_definition,
+            f'(assert (not (=> (and (inv-f {entry_args}) trans-f) (inv-f {exit_args}))))',
+            '(check-sat)'
+        ])
+        post_vc = '\n'.join(base + [
+            smt_post_fun_definition,
+            f'(assert (not (=> (inv-f {exit_args}) post-f)))',
+            '(check-sat)'
+        ])
 
         return pre_vc, trans_vc, post_vc
 
@@ -128,14 +143,17 @@ class Generator:
                 if isinstance(ir, OperationWithLValue) and ir.lvalue:
                     base_name = self._get_base_name(ir.lvalue)
                     if not base_name:
-                        raise NoneTypeVarName()
+                        continue
                     loop_var_names.add(base_name)
                     exit_ssa_map[base_name] = self._var_to_smt(ir.lvalue)
+                    if isinstance(ir, Phi) and ir.rvalues:
+                        entry_var = sorted(ir.rvalues, key=lambda v: self._var_to_smt(v))[0]
+                        entry_ssa_map[base_name] = self._var_to_smt(entry_var)
                     if hasattr(ir, 'read') and ir.read:
                         for var in ir.read:
                             if self._get_base_name(var) == base_name:
-                                entry_ssa_map[base_name] = self._var_to_smt(var)
-        
+                                entry_ssa_map.setdefault(base_name, self._var_to_smt(var))
+
         # Handle variable that are only read within the loop body
         unmapped_vars = {v for v in loop_var_names if v not in entry_ssa_map}
         for node in trans_path:
@@ -212,6 +230,8 @@ class Generator:
     def _get_base_name(self, var: Variable) -> str|None:
         if isinstance(var, Constant):
             return str(var.value)
+        if isinstance(var, TemporaryVariable):
+            return None
         if hasattr(var, 'non_ssa_version') and var.non_ssa_version:
             return var.non_ssa_version.name
         if var.name:
