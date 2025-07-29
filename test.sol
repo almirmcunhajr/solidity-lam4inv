afd06ef49d30ceef242f8ead7272f0a26814c872
Almir Cunha <almir.cunha@incognia.com>
2025-07-29 07:51:29 -0300

    Change test example

--- test.sol
+++ test.sol
@@ -1,21 +1,19 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 contract LoopExample {
-    uint256 public x;
-    uint256 public y;
+    int x;
+    int y;
 
     constructor() {
-        // pre-conditions
         x = 1;
         y = 0;
 
-        // loop body
-        while (y < 100000) {
-            x = x + y;
+        while (y < 1024) {
+            x = 0;
             y = y + 1;
         }
 
-        // post-condition
-        assert(x >= y);
+       assert(x == 0);
     }
 }
