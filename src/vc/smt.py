7f8545c6499ebc8691187ce716404e6f97e1dddb
Almir Cunha <almir.cunha@incognia.com>
2025-09-27 11:49:17 -0300

    Pretty print SMT

--- src/vc/smt.py
+++ src/vc/smt.py
@@ -9,14 +9,32 @@ class Op:
     def __hash__(self) -> int:
         return hash(str(self))
 
+    def needs_expansion(self) -> bool:
+        raise NotImplementedError()
+
+    def pretty(self, indent_level: int = 0) -> str:
+        raise NotImplementedError()
+
 class UnaryOp(Op):
     arg: str|Op
 
     def __init__(self, arg: str|Op):
         self.arg = arg
 
+    def needs_expansion(self) -> bool:
+        return isinstance(self.arg, Op) and self.arg.needs_expansion()
+
+    def pretty(self, indent_level: int = 0) -> str:
+        indent = '  ' * indent_level
+
+        if not self.needs_expansion():
+            return f'{indent}( {self._op} {self.arg} )'
+
+        inner = self.arg.pretty(indent_level=indent_level+1)
+        return f'{indent}( {self._op}\n{inner}\n{indent})'
+
     def __str__(self) -> str:
-        return f'({self._op} {self.arg})'
+        return self.pretty()
 
 class BinaryOp(Op):
     left: str|Op
@@ -26,8 +44,22 @@ class BinaryOp(Op):
         self.left = left
         self.right = right
 
+    def needs_expansion(self) -> bool:
+        return (isinstance(self.left, Op) and self.left.needs_expansion()) or (isinstance(self.right, Op) and self.right.needs_expansion())
+
+    def pretty(self, indent_level: int = 0) -> str:
+        indent = '  ' * indent_level
+
+        if not self.needs_expansion():
+            return f'{indent}( {self._op} {self.left} {self.right} )'
+
+        left = self.left.pretty(indent_level=indent_level+1) if isinstance(self.left, NaryOp) else f'{indent}  {self.left}'
+        right = self.right.pretty(indent_level=indent_level+1) if isinstance(self.right, NaryOp) else f'{indent}  {self.right}'
+
+        return f'{indent}( {self._op}\n{left}\n{right}\n{indent})'
+
     def __str__(self) -> str:
-        return f'({self._op} {self.left} {self.right})'
+        return self.pretty()
 
 class NaryOp(Op):
     args: list[str|Op]
@@ -35,8 +67,23 @@ class NaryOp(Op):
     def __init__(self, *args: str|Op):
         self.args = list(args)
 
+    def needs_expansion(self) -> bool:
+        return any(isinstance(arg, Op) for arg in self.args)
+
+    def pretty(self, indent_level: int = 0) -> str:
+        indent = '  ' * indent_level
+
+        if len(self.args) == 0:
+            return ''
+
+        if not self.needs_expansion():
+            return f'{indent}( {self._op} {" ".join([str(arg) for arg in self.args])} )'
+
+        inner = f'\n'.join([str(arg) if isinstance(arg,str) else arg.pretty(indent_level=indent_level+1) for arg in self.args])
+        return f'{indent}( {self._op}\n{inner}\n{indent})'
+
     def __str__(self) -> str:
-        return f'({self._op} {" ".join([str(arg) for arg in self.args])})'
+        return self.pretty()
 
     def append(self, arg: str|Op):
         self.args.append(arg)
