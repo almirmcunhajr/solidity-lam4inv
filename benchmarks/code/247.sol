5bc853297b05bb2cffefb3d19956b0b1aa941889
Almir Cunha <almir.cunha@incognia.com>
2025-10-07 00:17:29 -0300

    Add results

--- benchmarks/code/247.sol
+++ benchmarks/code/247.sol
@@ -2,14 +2,14 @@
 pragma solidity ^0.8.0;
 
 contract LoopExample {
-    constructor(int idBitLength, int material_length, int nlen, int j, int k) {
+    constructor(int idBitLength, int materialLength, int nlen, int j, int k) {
         require(nlen == idBitLength / 32, "failed pre-condition");
         require(idBitLength >= 0, "failed pre-condition");
-        require(material_length >= 0, "failed pre-condition");
+        require(materialLength >= 0, "failed pre-condition");
         j = 0;
-        while ((j < idBitLength / 8) && (j < material_length)) {
+        while ((j < idBitLength / 8) && (j < materialLength)) {
             j = j + 1;
         }
-        assert(j <= material_length);
+        assert(j <= materialLength);
     }
 }
